149e95a5c5414f79e902d7f3f8dcbf97a6c28fa4
MacUser <>
2023-03-09 20:23:02 +0000

    Initial commit

---
+++ Movie Lister Example/Movie_Lister_ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Movie_Lister_ExampleApp.swift
+//  Movie Lister Example
+//
+//  Created by MacUser on 09/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct Movie_Lister_ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
